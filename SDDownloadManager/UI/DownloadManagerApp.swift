017f87b909da1e98b1e3eb4219b68f85136fe629
UltraGeek0102 <Dhruv.bosamia@gmail.com>
2026-06-22 15:27:10 +0530

    Create DownloadManagerApp.swift

---
+++ SDDownloadManager/UI/DownloadManagerApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+import UserNotifications
+
+@main
+struct DownloadManagerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
